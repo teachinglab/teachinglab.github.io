d872453e0c712de28770ca763a401b07df81754d
dungates <duncan.gates123@gmail.com>
2021-12-09 09:37:26 -0800

    fix image files

--- docs/R/image_maker.R
+++ docs/R/image_maker.R
@@ -5,6 +5,6 @@ all_files <- list.files(here::here("2022Reports"), full.names = T)
 
 all_files_png <- list.files(here::here("2022Reports")) %>% str_replace_all(., "\\.html", "\\.png")
   
-map2(all_files, all_files_png, ~ webshot(url = .x, 
+walk2(all_files, all_files_png, ~ webshot(url = .x, 
                                         file = paste0(here::here("Images/Reports/Midyear/"), .y),
                                         cliprect = "viewport"))
