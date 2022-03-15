d1e69477fdb06e44f880f439c52ddca27a777f6b
dungates <duncan.gates123@gmail.com>
2022-03-15 10:48:00 -0500

    add north andover

--- docs/R/image_maker.R
+++ docs/R/image_maker.R
@@ -3,8 +3,9 @@ library(webshot)
 
 all_files <- list.files(here::here("2022Reports"), full.names = T)
 
-all_files_png <- list.files(here::here("2022Reports")) %>% str_replace_all(., "\\.html", "\\.png")
+all_files_png <- list.files(here::here("2022Reports")) %>% 
+  stringr::str_replace_all(., "\\.html", "\\.png")
   
-walk2(all_files, all_files_png, ~ webshot(url = .x, 
+purrr::walk2(all_files, all_files_png, ~ webshot(url = .x, 
                                         file = paste0(here::here("Images/Reports/Midyear/"), .y),
                                         cliprect = "viewport"))
