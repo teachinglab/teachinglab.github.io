93bf41b4f2afdbbf2f25e06fbc115e09784e4cc6
dungates <duncan.gates123@gmail.com>
2022-03-17 18:31:54 -0400

    add all misssissippi reports

--- R/image_maker.R
+++ R/image_maker.R
@@ -1,11 +1,11 @@
 library(tidyverse)
 library(webshot)
 
-all_files <- list.files(here::here("2022Reports"), full.names = T)
+all_files <- list.files("~/Teaching Lab/Coding/TeachingLab/Analysis/2021-2022/Mississippi/backup_reports", full.names = T)
 
-all_files_png <- list.files(here::here("2022Reports")) %>% 
+all_files_png <- list.files("~/Teaching Lab/Coding/TeachingLab/Analysis/2021-2022/Mississippi/backup_reports") %>% 
   stringr::str_replace_all(., "\\.html", "\\.png")
   
 purrr::walk2(all_files, all_files_png, ~ webshot(url = .x, 
-                                        file = paste0(here::here("Images/Reports/Midyear/"), .y),
+                                        file = paste0(here::here("Images/Reports/Mississippi/"), .y),
                                         cliprect = "viewport"))
