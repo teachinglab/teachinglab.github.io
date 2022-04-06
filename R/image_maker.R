15d274b11cf95fad556f8d2855c71a4f0e1243f2
dungates <duncan.gates123@gmail.com>
2022-04-06 13:11:18 -0700

    update mcnairy

--- R/image_maker.R
+++ R/image_maker.R
@@ -4,13 +4,15 @@ library(webshot)
 save_screenshot_report <- function(files_input_loc, files_output_loc) {
   all_files <- list.files(files_input_loc, full.names = T)
   
-  all_files_png <- list.files("~/Teaching Lab/Coding/TeachingLab/Analysis/2021-2022/Mississippi/backup_reports") %>% 
+  all_files_png <- list.files(files_input_loc) %>% 
     stringr::str_replace_all(., "\\.html", "\\.png")
   
   purrr::walk2(all_files, all_files_png, ~ webshot(url = .x, 
                                                    file = paste0(here::here(files_output_loc), .y),
                                                    cliprect = "viewport"))
 }
+
+### This workflow needs fixing ###
 
 save_screenshot_report(files_input_loc = "~/Teaching Lab/Coding/TeachingLab/Analysis/2021-2022/Mid-Year Report/Mid-Year Report",
                        files_output_loc = "Images/Reports/Midyear/")
