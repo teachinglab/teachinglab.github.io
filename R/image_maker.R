0519b861f0cda6e77738e5a41746d1f77892eba1
dungates <duncan.gates123@gmail.com>
2022-04-06 12:13:29 -0700

    update and add mcnairy mid year report

--- R/image_maker.R
+++ R/image_maker.R
@@ -1,11 +1,16 @@
 library(tidyverse)
 library(webshot)
 
-all_files <- list.files("~/Teaching Lab/Coding/TeachingLab/Analysis/2021-2022/Mississippi/backup_reports", full.names = T)
-
-all_files_png <- list.files("~/Teaching Lab/Coding/TeachingLab/Analysis/2021-2022/Mississippi/backup_reports") %>% 
-  stringr::str_replace_all(., "\\.html", "\\.png")
+save_screenshot_report <- function(files_input_loc, files_output_loc) {
+  all_files <- list.files(files_input_loc, full.names = T)
   
-purrr::walk2(all_files, all_files_png, ~ webshot(url = .x, 
-                                        file = paste0(here::here("Images/Reports/Mississippi/"), .y),
-                                        cliprect = "viewport"))
+  all_files_png <- list.files("~/Teaching Lab/Coding/TeachingLab/Analysis/2021-2022/Mississippi/backup_reports") %>% 
+    stringr::str_replace_all(., "\\.html", "\\.png")
+  
+  purrr::walk2(all_files, all_files_png, ~ webshot(url = .x, 
+                                                   file = paste0(here::here(files_output_loc), .y),
+                                                   cliprect = "viewport"))
+}
+
+save_screenshot_report(files_input_loc = "~/Teaching Lab/Coding/TeachingLab/Analysis/2021-2022/Mid-Year Report/Mid-Year Report",
+                       files_output_loc = "Images/Reports/Midyear/")
--- docs/R/image_maker.R
+++ docs/R/image_maker.R
@@ -1,11 +1,11 @@
 library(tidyverse)
 library(webshot)
 
-all_files <- list.files("~/Teaching Lab/Coding/TeachingLab/Analysis/2021-2022/Mississippi/backup_reports", full.names = T)
+all_files <- list.files("~/Downloads/2021-2022-tl-report_McNairy County, TN.html", full.names = T)
 
 all_files_png <- list.files("~/Teaching Lab/Coding/TeachingLab/Analysis/2021-2022/Mississippi/backup_reports") %>% 
   stringr::str_replace_all(., "\\.html", "\\.png")
   
 purrr::walk2(all_files, all_files_png, ~ webshot(url = .x, 
-                                        file = paste0(here::here("Images/Reports/Mississippi/"), .y),
+                                        file = paste0(here::here("Images/Reports/Midyear/"), .y),
                                         cliprect = "viewport"))
